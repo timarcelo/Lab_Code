83702a577f702bc2f32829f77bf071d7a7f8892e
timarcelo <78170752+timarcelo@users.noreply.github.com>
2024-07-13 09:52:41 -0300

    Atualizar 'main.ts'

--- main.ts
+++ main.ts
@@ -7,7 +7,7 @@ const enum DistanceUnit {
 
 //% color=#0fbc11 icon="\u272a" block="<LAB_CODE>"
 //% category="MakerBit"
-namespace < LAB_CODE > {
+namespace LAB_CODE {
 //    const MICROBIT_LABCODE_ULTRASONIC_OBJECT_DETECTED_ID = 798;
   //  const MAX_ULTRASONIC_TRAVEL_TIME = 300 * DistanceUnit.CM;
   //  const ULTRASONIC_MEASUREMENTS = 3;
