c2a38068717495403ddf29b486a5a712701dea46
timarcelo <78170752+timarcelo@users.noreply.github.com>
2024-07-13 09:47:30 -0300

    Atualizar 'main.ts'

--- main.ts
+++ main.ts
@@ -34,98 +34,95 @@ namespace SeguidordeLinha {
         whiteLeft = getFilteredReading(leftSensorPin, true);
         whiteCenter = getFilteredReading(centerSensorPin, true);
         whiteRight = getFilteredReading(rightSensorPin, true);
-        
-        }
-        basic.showString("B");
-        while (!input.buttonIsPressed(Button.B)) {
-            basic.pause(100);
-        }
-    
-        blackLeft = getFilteredReading(leftSensorPin, true);
-        blackCenter = getFilteredReading(centerSensorPin, true);
-        blackRight = getFilteredReading(rightSensorPin, true);
 
-     basic.showString("W");
-        while (!input.buttonIsPressed(Button.A)+!input.buttonIsPressed(Button.B)) {
-            basic.pause(100);
-        }
-        basic.showIcon(IconNames.Yes);
+    }
+    basic.showString("B");
+    while (!input.buttonIsPressed(Button.B)) {
+        basic.pause(100);
     }
 
-    //% blockId=Seguidor_de_Linha_read_left block="grave sensor esquerdo"
-    //% weight=80
-    export function readLeftSensor(): number {
-        return Math.round(getFilteredReading(leftSensorPin, false));
-    }
+    blackLeft = getFilteredReading(leftSensorPin, true);
+    blackCenter = getFilteredReading(centerSensorPin, true);
+    blackRight = getFilteredReading(rightSensorPin, true);
 
-    //% blockId=Seguidor_de_Linha_read_right block="grave sensor direito"
-    //% weight=80
-    export function readRightSensor(): number {
-        return Math.round(getFilteredReading(rightSensorPin, false));
+    basic.showString("C");
+    while (!input.buttonIsPressed(Button.A + B) + !input.buttonIsPressed(Button.B)) {
+        basic.pause(100);
     }
- //% blockId=Seguidor_de_Linha_read_center block="grave sensor central"
-    //% weight=80
-    export function readRightSensor(): number {
-        return Math.round(getFilteredReading(centerSensorPin, false));
+    basic.showIcon(IconNames.Yes);
+}
+
+//% blockId=Seguidor_de_Linha_read_left block="grave sensor esquerdo"
+//% weight=80
+export function readLeftSensor(): number {
+    return Math.round(getFilteredReading(leftSensorPin, false));
+}
+
+//% blockId=Seguidor_de_Linha_read_right block="grave sensor direito"
+//% weight=80
+export function readRightSensor(): number {
+    return Math.round(getFilteredReading(rightSensorPin, false));
+}
+//% blockId=Seguidor_de_Linha_read_center block="grave sensor central"
+//% weight=80
+export function readRightSensor(): number {
+    return Math.round(getFilteredReading(centerSensorPin, false));
+}
+
+//% blockId=Seguidor_de_Linha_is_on_line block="usar sensor %sensor"
+//% weight=70
+export function isOnLine(sensor: SeguidordeLinhaSensor): boolean {
+    let sensorValue: number;
+    let whiteValue: number;
+    let blackValue: number;
+
+    if (sensor === SeguidordeLinhaSensor.Left) {
+        sensorValue = Math.round(getFilteredReading(leftSensorPin, false));
+        whiteValue = whiteLeft;
+        blackValue = blackLeft;
     }
+   // else {
+     //   sensorValue = Math.round(getFilteredReading(centerSensorPin, false));
+      //  whiteValue = whiteRight;
+       // blackValue = blackRight;
+   // }
+      else {
+        sensorValue = Math.round(getFilteredReading(rightSensorPin, false));
+        whiteValue = whiteRight;
+        blackValue = blackRight;
+    }
+    return (sensorValue > whiteValue && sensorValue < blackValue);
+}
 
-    //% blockId=Seguidor_de_Linha_is_on_line block="usar sensor %sensor"
-    //% weight=70
-    export function isOnLine(sensor: SeguidordeLinhaSensor): boolean {
-        let sensorValue: number;
-        let whiteValue: number;
-        let blackValue: number;
+function getFilteredReading(pin: AnalogPin, isCalibration: boolean): number {
+    let currentValue = pins.analogReadPin(pin);
 
-        if (sensor === SeguidordeLinhaSensor.Left) {
-            sensorValue = Math.round(getFilteredReading(leftSensorPin, false));
-            whiteValue = whiteLeft;
-            blackValue = blackLeft;
+    if (pin === leftSensorPin) {
+        if (isCalibration) {
+            leftSensorValue = currentValue;
         } else {
-            sensorValue = Math.round(getFilteredReading(centerSensorPin, false));
-            whiteValue = whiteRight;
-            blackValue = blackRight;
+            leftSensorValue = ALPHA * currentValue + (1 - ALPHA) * leftSensorValue;
         }
-          else {
-            sensorValue = Math.round(getFilteredReading(rightSensorPin, false));
-            whiteValue = whiteRight;
-            blackValue = blackRight;
-        }
-        return (sensorValue > whiteValue && sensorValue = blackValue && sensorValue < blackValue);
-    }
-
-    function getFilteredReading(pin: AnalogPin, isCalibration: boolean): number {
-        let currentValue = pins.analogReadPin(pin);
-
-        if (pin === leftSensorPin) {
-            if (isCalibration) {
-                leftSensorValue = currentValue;
-            } else {
-                leftSensorValue = ALPHA * currentValue + (1 - ALPHA) * leftSensorValue;
-            }
-            return leftSensorValue;
+        return leftSensorValue;
+    } else {
+        if (isCalibration) {
+            rightSensorValue = currentValue;
         } else {
-            if (isCalibration) {
-                rightSensorValue = currentValue;
-            } else {
-                rightSensorValue = ALPHA * currentValue + (1 - ALPHA) * rightSensorValue;
-            }
-            return rightSensorValue;
+            rightSensorValue = ALPHA * currentValue + (1 - ALPHA) * rightSensorValue;
         }
-    }
-
-    // Enum for sensors
-    export enum SeguidordeLinhaSensor {
-        //% block="esquerdo"
-        Esquerdo,
-          //% block="central"
-        Central,
-        //% block="direito"
-        Direito
+        return rightSensorValue;
     }
 }
 
-            basic.pause(duration);
-            basic.clearScreen();
-        }
-    }
+// Enum for sensors
+export enum SeguidordeLinhaSensor {
+    //% block="esquerdo"
+    Esquerdo,
+    //% block="central"
+    Central,
+    //% block="direito"
+    Direito
 }
+
+basic.pause(duration);
+basic.clearScreen();
